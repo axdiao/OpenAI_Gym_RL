d0eeda82e644632f9a3f86f868445fe6e79296dd
axidao <axdiao@umich.edu>
2020-05-04 19:47:30 -0400

    Initial commit

---
+++ cart_pole.py
@@ -0,0 +1,24 @@
+import gym
+
+
+def main():
+    print('Cart Pole')
+    env = gym.make('CartPole-v1')
+
+    for i in range(20):
+        print('Episode', i)
+        env.reset()
+        reward = 0
+        done = False
+        while not done:
+            env.render()
+            _, r, done, _ = env.step(env.action_space.sample())
+            reward += r
+        print('Reward:', reward)
+        print('')
+
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
